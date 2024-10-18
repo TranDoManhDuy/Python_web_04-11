0e4e65fd70d896288eb777b952b44080561a818a
tranmanhduy <yourusername@users.noreply.github.com>
2024-10-18 13:53:28 +0700

    update LOGIN

--- WebPython/login.py
+++ WebPython/login.py
@@ -1,5 +1,16 @@
 import rootflaskapp
 
+emailAdmin = "abc"
+passwordAdmin = "123"
+
+@rootflaskapp.app.route("/login_post", methods=["POST"])
+def login_post():
+    data = rootflaskapp.request.get_json()
+    print(data)
+    if data["email"] == emailAdmin and data["password"] == passwordAdmin:
+        return rootflaskapp.redirect("/statistical")
+    return rootflaskapp.jsonify("Login failed")
+
 @rootflaskapp.app.route("/login")
 def login():
     return rootflaskapp.render_template("views/login.html")
--- WebPython/static/handle/login.js
+++ WebPython/static/handle/login.js
@@ -1,41 +1,30 @@
-// let submitFormBtn = document.querySelector('#submitFormBtn')
-// let exampleInputEmail = document.querySelector('#exampleInputEmail')
-// let exampleInputPassword = document.querySelector('#exampleInputPassword')
-// let customCheck = document.querySelector('#customCheck')
+let inputEmail = document.querySelector('#exampleInputEmail')
+let inputPassword = document.querySelector('#exampleInputPassword')
+let btnSubmit = document.querySelector('#submitFormBtn')
+let btnRememberLogin = document.querySelector('#customCheck')
+btnSubmit.addEventListener('click', function() {
+    fetch('/login_post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: inputEmail.value,
+            password: inputPassword.value,
+            remember: btnRememberLogin.checked
+        })
+    })
+    .then(response => {
+        if (response.redirected) {
+            window.location.href = response.url;  // Chuyển hướng theo URL trả về
+        }
+        else {
+            inputEmail.value = ''
+            inputPassword.value = ''
+            return response.json()
+        }
+    })
+    .then(data => console.log(data))
+})
 
-// console.log(submitFormBtn)
-// console.log(exampleInputEmail)
-// console.log(exampleInputPassword)
-
-// function chuyentrang() {
-//     window.location.href = "/home"
-// }
-
-// submitFormBtn.addEventListener('click', function() {
-//     let email = exampleInputEmail.value
-//     let password = exampleInputPassword.value
-//     let check = customCheck.checked
-//     let data = {
-//         email: email,
-//         password: password,
-//         check: check
-//     }
-//     console.log(data)
-    
-//     fetch("/loginHandle", {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     }).then(res => {
-//         console.log(res)
-//         if (res.status === 200) {
-//             chuyentrang()
-//         }
-//         return res.json()
-//     }
-//     ).then(data => {
-//         console.log(data)
-//     })
-// })
+console.log(inputEmail, inputPassword, btnSubmit)
