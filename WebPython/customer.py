f1cc6847080ada704b8cd026914be2b1134f8149
tranmanhduy <yourusername@users.noreply.github.com>
2024-10-26 15:28:37 +0700

    delete

--- WebPython/customer.py
+++ WebPython/customer.py
@@ -6,24 +6,6 @@ import re
 # ho tro tim kiem
 # lấy dữ liệu từ sql (tạm ổn)
 
-# /////////////////////sql////////////////////////////////////
-
-
-def takedata(lenh):  # lenh truy van sql
-    with sqlite3.connect('WebPython/data.db') as conn:
-        c = conn.cursor()
-        c.execute(lenh)
-        respon = c.fetchall()
-        return respon
-
-def pushdata(lenh):  # lenh truy van sql
-    with sqlite3.connect('WebPython/data.db') as conn:
-        c = conn.cursor()
-        c.execute(lenh)
-        conn.commit()
-        return True
-# ////////////////////////////////////////////////////////////
-
 # /////////////////////khach hang////////////////////////////////////
 
 
