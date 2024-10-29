3840ed4cea06496cbfeda8705853891b3300b9d2
nyvantran <tondaithanh0@gmail.com>
2024-10-29 09:50:03 +0700

    2

--- WebPython/datacenter.py
+++ WebPython/datacenter.py
@@ -1,6 +1,9 @@
 import sqlite3
-
-
+# sql = sqlite3.connect('WebPython/data.db')
+# cur = sql.cursor()
+# cur.execute('DELETE FROM CUSTOMER WHERE ID = "000000005"')
+# sql.commit()
+# sql.close()
 def takedata(lenh):  # lenh truy van sql
     with sqlite3.connect('WebPython/data.db') as conn:
         c = conn.cursor()
